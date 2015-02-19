ee3bf1b00f841b17658c719acd0a2e64d0fc8d8f
EtienneWan <etienne.wan@yahoo.com>
2015-02-19 12:33:50 +0000

    Ajout de la fonction sendFreeSMS

--- cron.php
+++ cron.php
@@ -6,7 +6,7 @@
  * voir les constantes ci-dessous
  */
 
-define('KEY', 'insert-random-secret-key-here');  // Clé servant à protéger l'accès à ce fichier
+define('KEY', 'blabla123');  // Clé servant à protéger l'accès à ce fichier
 define('INTERVAL', 600); // Interval entre chaque "Ping"
 define('CRITICAL_INTERVAL', 60); // Interval en cas de "pré-down" (voir plus bas
 
@@ -15,7 +15,7 @@ define('CRITICAL_INTERVAL', 60); // Interval en cas de "pré-down" (voir plus ba
  * NB: J'utilise PushBullet, mais vous pouvez modifier
  * ce code pour utiliser votre propre service voir
  * votre propre application.
- */ 
+ */
 function sendPushbulletNotification($title, $content='') {
 	$postdata = json_encode(array(
 		'type' => 'note',
@@ -26,15 +26,35 @@ function sendPushbulletNotification($title, $content='') {
 	shell_exec("curl -u YOUR-PUSHBULLET-KEY: -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary '$postdata'");
 }
 
+function sendFreeSMS($content){
+	/* L'API SMS de Free permet d'envoyer des SMS d'alertes gratuitement sur votre numéro de portable.
+	C'est une option gratuite à activer sur http://mobile.free.fr, se connecter, puis mes options, et
+	activer "Notifications par SMS".
+	Rentrer ci-dessous votre identifiant de compte et la clé de sécurité fournie, et vous serez prêts
+	à recevoir des SMS.
+	*/
+	$url = 'https://smsapi.free-mobile.fr/sendmsg';
+	$paramUser = ""; //$paramUser -> votre identifiant à 8 chiffres (
+	$paramPass = ""; //$paramPass -> clé alphanumérique d'identification au service.
+	$paramMsg  = "[PingPong] ".$content; // Commodité qui me permet de différencier quel programme m'envoie un SMS, car plusieurs de mes programmes utilisent cette API.
+
+	$postdata = json_encode(array(
+		"user" => $paramUser,
+		"pass" => $paramPass,
+		"msg"  => $paramMsg
+		));
+	shell_exec("curl -k -X POST $url --header 'Content-Type: application/json' --data-binary '$postdata'");
+}
+
 // Empêche le script d'être lancé via le web (CLI seulement)
 if (isset($argc, $argv)) {
-	
+
 	// Si la clé donnée en argument est correcte
 	if (isset($argv[1]) && $argv[1] == KEY) {
 		require_once 'system/Database.php';
 		require_once 'system/Utils.php';
 		$bdd = new Database();
-		
+
 		$req = $bdd->query("SELECT * FROM service WHERE activated=1");
 		// Pour chaque service
 		foreach ($req as $res) {
@@ -42,39 +62,39 @@ if (isset($argc, $argv)) {
 			$host = $res['host'];
 			$port = $res['port'];
 			$timeout = 5;
-			
+
 			echo 'Ping ('.$host.':'.$port.') ... ';
-			
+
 			// Si on a dépassé la date de prochain passage pour le service courant
 			if (Utils::time() > $res['next_checking_timestamp']) {
-				
+
 				// On teste la réponse du service (le fameux "Ping)
 				if (!($fp = @fsockopen($host, $port, $errStr, $timeout))) {
 					// Down
 					 echo '**DOWN**';
-					
+
 					switch ($res['status']) {
 						// Si le service est "En attente"
 						case 2:
 							// C'est VRAIMENT down, modification du statut en "DOWN", on reviendra checker dans INTERVAL
 							$nextCheckTimestamp = Utils::time() + INTERVAL;
 							$bdd->query("UPDATE service SET status=3, next_checking_timestamp=$nextCheckTimestamp WHERE id=$id");
-							
+
 							// Envoi de la notif de DOWN
 							sendPushbulletNotification($res['name'].': **DOWN**');
 						break;
-						
+
 						// Si le service est déjà down
 						case 3:
 							// On se contente de re-attrendre INTERVAL
 							$nextCheckTimestamp = Utils::time() + INTERVAL;
 							$bdd->query("UPDATE service SET next_checking_timestamp=$nextCheckTimestamp WHERE id=$id");
 						break;
-						
+
 						// Dans tous les autres cas (indéterminé ou opérationnel)
 						default:
 							// On passe "En attente" (le fameux pré-down), et on reprogramme un passage dans CRITICAL_INTERVAL
-							
+
 							/*
 							 * Cette technique est utilisé pour prévenir des "mini-drops"
 							 * qui peuvent parvenir sur vos services
@@ -88,19 +108,19 @@ if (isset($argc, $argv)) {
 				}
 				else {
 					// Up
-					
+
 					// Après le Ping, le Pong...
 					echo 'Pong !';
-					
+
 					// Programmation du prochain passage dans INTERVAL
 					$nextCheckTimestamp = Utils::time() + INTERVAL;
-					
+
 					// Si le service était DOWN
 					if ($res['status'] == 3) {
 						// Envoi de la notif d'UP
 						sendPushbulletNotification($res['name'].': **UP**');
 					}
-					
+
 					$bdd->query("UPDATE service SET status=1, next_checking_timestamp=$nextCheckTimestamp WHERE id=$id");
 				}
 			}
@@ -108,7 +128,7 @@ if (isset($argc, $argv)) {
 				// Ce n'est pas encore le tour de ce service
 				echo 'Not yet !';
 			}
-				
+
 			echo "\n";
 		}
 	}
